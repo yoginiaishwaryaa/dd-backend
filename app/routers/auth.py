b75e13899eb334ff30482c3b361b1fd2469ebf8a
Yogini Aishwaryaa P T S <yoginianithakumar@gmail.com>
2026-03-10 17:41:45 +0530

    fix: auth for cross platform

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -55,6 +55,8 @@ def create_user(
         key="access_token",
         value=access_token,
         httponly=True,
+        samesite="none",
+        secure=True,
         max_age=int(access_token_expires.total_seconds()),
         expires=int(access_token_expires.total_seconds()),
     )
@@ -63,6 +65,8 @@ def create_user(
         key="refresh_token",
         value=refresh_token,
         httponly=True,
+        samesite="none",
+        secure=True,
         max_age=int(refresh_token_expires.total_seconds()),
         expires=int(refresh_token_expires.total_seconds()),
     )
@@ -99,6 +103,8 @@ def login(response: Response, user_in: schemas.UserLogin, db: Session = Depends(
         key="access_token",
         value=access_token,
         httponly=True,
+        samesite="none",
+        secure=True,
         max_age=int(access_token_expires.total_seconds()),
         expires=int(access_token_expires.total_seconds()),
     )
@@ -107,6 +113,8 @@ def login(response: Response, user_in: schemas.UserLogin, db: Session = Depends(
         key="refresh_token",
         value=refresh_token,
         httponly=True,
+        samesite="none",
+        secure=True,
         max_age=int(refresh_token_expires.total_seconds()),
         expires=int(refresh_token_expires.total_seconds()),
     )
